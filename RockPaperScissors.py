f08ea7ee88d37793f80d9950bc2a3ce0a31009f7
JimmyBJammin <james_barton18@yahoo.com>
2025-10-22 13:50:34 -0600

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -71,33 +71,10 @@ while playAgain == 'y' or playAgain == 'Y':
             print("Rock smashes scissors. You lose! :(\n")
             loses =+ 1
 
-
+    #Print the current totals
     print("Wins/Draws/Loses")
     print(str(wins) + "/" + str(draws) + "/" + str(loses) + "\n")
 
     #Will end the loop if the user enters anything other than 'y' or 'Y'
     playAgain = input("Do you want to play again? y/n -> ")
     print()
-"""
-    #If user doesn't pick 'r', 'p', 's' - end, otherwise, output outcome
-    if not(player == 'r' or player == 'p' or player == 's'):
-        print("Sorry, that wasn't one of the valid choices. Choose 'r', 'p', or 's' and try again later.")
-    elif computer == player:
-        print("DRAW!")
-    elif computer == 'r':
-        if player == 'p':
-            print("Paper covers rock. You win!")
-        else:
-            print("Rock smashes scissors. Sorry, you lose!")
-    elif computer == 'p':
-        if player == 'r':
-            print("Paper covers rock. Sorry, you lose!")
-        else:
-            print("Scissors cut paper. You win!")
-    elif computer == 's':
-        if player == 'r':
-            print("Rock smashes scissors. You win!")
-        else:
-            print("Scissors cut paper. Sorry, you lose!")
-
-"""
